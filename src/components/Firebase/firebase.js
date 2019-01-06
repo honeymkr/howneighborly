6395dbb8119d70edf11177ad42234232ad5c2164
Jasper Fawkes <design@honeymkr.com>
2019-01-06 12:16:24 -0500

    updated env vars

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,11 +1,12 @@
 
 const config = {
-  apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    databaseURL: DATABASE_URL,
-    projectId: PROJECT_ID,
-    storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGING_SENDER_ID
+  apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+
 };
 
 class Firebase {
