d32a836240b82e8bb12a842171c5c858f0e28a7d
Jasper Fawkes <design@honeymkr.com>
2019-01-08 12:38:13 -0500

    typo tweak

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       {
         resolve: `gatsby-plugin-typography`,
         options: {
-          pathToConfigModule: `src/components/utils/typography.js`,
+          pathToConfigModule: `src/components/Utils/typography.js`,
         },
       },
 
